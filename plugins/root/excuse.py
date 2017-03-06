97a620a98eeb91b05c01729c6acc54c9526bb958
Sylvia Petzanova <unspotted@gmail.com>
2017-03-06 03:14:02 +0200

    Add BOFH excuse plugin

---
+++ plugins/root/excuse.py
@@ -0,0 +1,12 @@
+# This plugin posts a random BOFH excuse
+# source: http://pages.cs.wisc.edu/~ballard/bofh/excuses
+
+def main(data):
+    if '!excuse' in data['recv']:
+        import random
+        import linecache
+        # hardcoded number of lines in excuse file
+        excuse = linecache.getline('bofh.txt', random.randint(0, 465))
+        return say(args['channel'], excuse)
+        
+
