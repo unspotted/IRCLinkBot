8804e44a9aa9111b8381a7345bd9bdd4c76ac707
Sylvia Petzanova <unspotted@gmail.com>
2017-03-06 04:51:47 +0200

    Fix excuse plugin

--- plugins/root/entertainment/excuse.py
+++ plugins/root/entertainment/excuse.py
@@ -2,11 +2,9 @@
 # source: http://pages.cs.wisc.edu/~ballard/bofh/excuses
 
 def main(data):
-    if '!excuse' in data['recv']:
-        import random
-        import linecache
+    if 'excuse' in data['recv']:
+        import random, linecache
+        args = argv('!excuse', data['recv'])
         # hardcoded number of lines in excuse file
         excuse = linecache.getline('bofh.txt', random.randint(0, 465))
         return say(args['channel'], excuse)
-        
-
